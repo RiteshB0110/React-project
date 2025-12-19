d9eb0fd0d1a4d5527756eb8a9095d39c464a00b4
Ritesh Bhosale <bhosaleritesh790@gmail.com>
2025-12-19 11:37:05 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,36 @@
+import './home.css';
+
+
+function Home() {
+    return (
+
+        <>
+
+
+
+
+<div>
+
+    {/* <h2 id='si'>Samyak institute</h2> */}
+    <img src="/comimg.jpg" alt="Computer"  />
+
+    <br></br>
+     <p >
+ <br></br>Samyak Computer Institute is a leading training and
+skill-development organization offering professional courses
+in IT, programming, designing, and vocational fields. With expert
+ trainers and industry-oriented curriculum, it helps students build
+strong technical foundations. The institute provides hands-on
+ learning, certifications, and career-focused training.
+ Samyak is known for its quality education, modern infrastructure,
+    and strong placement support. It aims to empower learners with
+skills that match today’s industry requirements.
+     </p>
+</div>
+
+
+</>
+
+    );
+}
+export default Home;
